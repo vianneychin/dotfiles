51c016f9c7f5a1a45e73035bfb73536f1b1de91f
vianneychin <vianneyjchin@gmail.com>
2024-11-07 02:15:39 -0800

    remove black borders around wezterm

--- .wezterm.lua
+++ .wezterm.lua
@@ -7,5 +7,11 @@ local config = wezterm.config_builder()
 config.font_size = 16
 config.enable_tab_bar = false
 config.window_decorations = "RESIZE"
+config.window_padding = {
+    left = 0,
+    right = 0,
+    top = 0,
+    bottom = 0
+}
 
 return config
